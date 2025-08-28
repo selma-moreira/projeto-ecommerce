8013c755355c16b9ff87593e9bef3f8e8c9dd8c3
Selma Moreira <selmamg18@gmail.com>
2025-08-28 14:50:18 -0300

    adiciona funcionalidade de atualizar valores do carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -25,10 +25,9 @@ botoesAdicionarAoCarrinho.forEach(botao => {
             };
             carrinho.push(produto);
         }
-        
+
         salvarProdutosNoCarrinho(carrinho);
-        atualizarContadorCarrinho();
-        renderizarTabelaDoCarrinho();
+        atualizarCarrinhoETabela();
     })
 });
 function salvarProdutosNoCarrinho(carrinho) {
@@ -51,8 +50,6 @@ function atualizarContadorCarrinho() {
     document.getElementById("contador-carrinho").textContent = total;
 }
 
-atualizarContadorCarrinho();
-
 function renderizarTabelaDoCarrinho() {
     const produtos = obterProdutosDoCarrinho();
     const corpoTabela = document.querySelector("#modal-1-content tbody");
@@ -67,27 +64,58 @@ function renderizarTabelaDoCarrinho() {
                                  <td>${produto.nome}</td>
                                 <td class="td-preco-unitario">R$ ${produto.preco.toFixed(2).replace(".", ",")}</td>
                                 <td class="td-quantidade">
-                                    <input type="number" value="${produto.quantidade}" min="1"></td>
-                                <td class="td-preco-total">R$ ${produto.preco.toFixed(2).replace(".", ",")} </td>
+                                    <input type="number" class="input-quantidade" data-id="${produto.id} value="${produto.quantidade}" min="1"></td>
+                                <td class="td-preco-total">R$ ${(produto.preco * produto.quantidade).toFixed(2).replace(".", ",")} </td>
                                 <td><button class="btn-remover" data-id=${produto.id} id="deletar"></button></td>`;
         corpoTabela.appendChild(tr);
     });
 }
 
-renderizarTabelaDoCarrinho();
-
 const corpoTabela = document.querySelector("modal-1-content table tbody");
 corpoTabela.addEventListener("click", evento => {
-    if(evento.target.classList.contains("btn-remover")) {
-            const id = evento.target.dataset.id;
-            removerProdutoDoCarrinho(id);
+    if (evento.target.classList.contains("btn-remover")) {
+        const id = evento.target.dataset.id;
+        removerProdutoDoCarrinho(id);
     }
-})
+});
+
+corpoTabela.addEventListener("input", evento => {
+    if (evento.target.classList.contains("input-quantidade")) {
+        const produtos = obterProdutosDoCarrinho();
+        const produto = produtos.find(produto => produto.id === evento.target.dataset.id);
+        let novaQuantidade = parseInt(evento.target.value);
+        if (produto) {
+            produto.quantidade = novaQuantidade;
+
+            salvarProdutosNoCarrinho(produtos);
+            atualizarCarrinhoETabela();
+        }
+    }
+});
 
 function removerProdutoDoCarrinho(id) {
     const produtos = obterProdutosDoCarrinho();
     const carrinhoAtualizado = produtos.filter(produto => produto.id !== id);
+
     salvarProdutosNoCarrinho(carrinhoAtualizado);
+    atualizarCarrinhoETabela();
+}
+
+function atualizarValorTotalCarrinho() {
+    const produtos = obterProdutosDoCarrinho();
+    let total = 0;
+
+    produtos.forEach(produto => {
+        total += produto.preco * produto.quantidade;
+    });
+
+    document.querySelector("#total-carrinho").textContent = `R$ ${total.toFixed(2).replace(".", ",")}`;
+}
+
+function atualizarCarrinhoETabela() {
     atualizarContadorCarrinho();
     renderizarTabelaDoCarrinho();
+    atualizarValorTotalCarrinho();
 }
+
+atualizarCarrinhoETabela();
